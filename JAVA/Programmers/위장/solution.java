390c10925a83ae343a88bd4da9af3494fa1eec9f
mwkangit <cabookis@gmail.com>
2021-12-21 21:05:43 +0900

    camouflage

---
+++ JAVA/Programmers/위장/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < clothes.length ; i++){
+            if(!map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], 1);
+            } else{
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+            }
+        }
+        
+        for(String s : map.keySet()){
+            answer *= map.get(s) + 1;
+        }
+        
+        
+        return answer - 1;
+    }
+}
