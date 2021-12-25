53deedc8063764a3114ca00e69bdb4857fcf5057
mwkangit <cabookis@gmail.com>
2021-12-25 20:40:08 +0900

    h-index

---
+++ JAVA/Programmers/H-Index/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        
+        for(int i = 0 ; i < citations.length ; i ++){
+            if(citations[i] >= citations.length - i){
+                answer = citations.length - i;
+                break;
+            }
+            
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}
