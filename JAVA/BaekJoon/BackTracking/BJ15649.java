62ea6edcea86670c1f3fb9fe05bd975b2176113f
mwkangit <cabookis@gmail.com>
2022-04-01 21:33:12 +0900

    BackTracking15649

---
+++ JAVA/BaekJoon/BackTracking/BJ15649.java
@@ -0,0 +1,53 @@
+package swmaestro;
+
+import java.util.*;
+import java.io.*;
+
+public class BJ15649 {
+
+	static StringBuilder sb = new StringBuilder();
+	static boolean visit[];
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		visit = new boolean[N];
+		
+		int arr[] = new int[N];
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = i+1;
+		}
+		
+		dfs(arr, M, 0, 0, new int[M]);
+		sb.deleteCharAt(sb.length()-1);
+		System.out.print(sb);
+
+	}
+	
+	static void dfs(int[] arr, int M, int d, int curr, int comp[]) {
+		if(d == M) {
+			for(int i = 0 ; i < M ; i++) {
+				sb.append(comp[i] + " ");
+			}
+			sb.deleteCharAt(sb.length()-1);
+			sb.append("\n");
+			
+		}else {
+			for(int i = curr ; i < arr.length ; i++) {
+				if(!visit[i]) {
+					visit[i] = true;	
+					comp[d] = arr[i];
+					dfs(arr, M, d+1, i+1, comp);
+					visit[i] = false;
+				}
+			}
+		}
+	}
+
+}
